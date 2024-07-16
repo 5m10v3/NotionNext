fd63a893547f0b6e39bccf4d7eb154c5df0c5e15
5m10v3 <87885422+5m10v3@users.noreply.github.com>
2024-07-16 11:34:13 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -26,7 +26,7 @@ const BLOG = {
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || '5m10v3', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || '目前主攻方向为Re', // 作者简介
-  LINK: process.env.NEXT_PUBLIC_LINK || 'https://tangly1024.com', // 网站地址
+  LINK: process.env.NEXT_PUBLIC_LINK || 'https://5m10v3.top', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'Notion, 博客', // 网站关键词 英文逗号隔开
 
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
